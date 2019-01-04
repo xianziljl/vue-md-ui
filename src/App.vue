d7a32870a87cb69dc798eca9958d6615699b92f3
zhuxianyong <zhuxianyong@eversec.cn>
2019-01-04 22:11:38 +0800

    项目搭建

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <m-button>Normal</m-button>
+      <m-button type="primary">Primary</m-button>
+      <m-button type="warning">Warning</m-button>
+      <m-button type="denger">Denger</m-button>
+      <m-button disabled type="primary">Primary</m-button>
+      <br>
+      <m-button outline>Normal</m-button>
+      <m-button outline type="primary">Primary</m-button>
+      <m-button outline type="warning">Warning</m-button>
+      <m-button outline type="denger">Denger</m-button>
+      <m-button disabled outline type="primary">Primary</m-button>
+      <br>
+      <m-button flat>Normal</m-button>
+      <m-button flat type="primary">Primary</m-button>
+      <m-button flat type="warning">Warning</m-button>
+      <m-button flat type="denger">Denger</m-button>
+      <m-button disabled flat type="primary">Primary</m-button>
+      <br>
+      <m-button round>Normal</m-button>
+      <m-button round type="primary">Primary</m-button>
+      <m-button round outline type="warning">Warning</m-button>
+      <m-button round flat type="denger">Denger</m-button>
+      <m-button round disabled outline type="primary">Primary</m-button>
+      <br>
+      <m-button size="small">Small</m-button>
+      <m-button size="small" type="primary" outline>
+        <m-icon>delete</m-icon>
+        <span>Icon</span>
+      </m-button>
+      <br>
+      <m-button>
+        <m-icon>delete</m-icon>
+        <span>图标</span>
+      </m-button>
+      <m-button type="primary">图标<m-icon>delete</m-icon></m-button>
+      <br>
+      <m-button size="large">
+        <m-icon>delete</m-icon>
+        <span>Icon Large</span>
+      </m-button>
+      <m-button size="large" type="primary">Large</m-button>
     </div>
-    <router-view/>
+    <!-- <m-icon size="20">delete</m-icon> -->
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+body{margin: 0;padding: 30px;font-size: 14px;}
+.m-button{margin: 5px;vertical-align: middle;}
 </style>
