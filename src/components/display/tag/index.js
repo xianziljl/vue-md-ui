766bb997defb3b404886532dd0ade6fcefcce101
zhuxianyong <zhuxianyong@eversec.cn>
2019-01-08 16:15:39 +0800

    新增 tag 组件

---
+++ src/components/display/tag/index.js
@@ -0,0 +1,7 @@
+import MTag from './tag'
+
+MTag.install = function (Vue) {
+  Vue.component(MTag.name, MTag)
+}
+
+export default MTag
---
+++ src/components/display/tag/tag.vue
@@ -0,0 +1,45 @@
+<template>
+<div
+  class="m-tag"
+  :class="{
+    'm-tag-close': close,
+    'm-tag-small': small,
+    'm-tag-outline': outline
+  }">
+  <slot></slot>
+  <i v-if="close" @click="$emit('close')" class="material-icons m-tag-close-btn">cancel</i>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'm-tag',
+  props: {
+    color: String,
+    close: Boolean,
+    small: Boolean,
+    outline: Boolean
+  }
+}
+</script>
+
+<style lang="less">
+.m-tag{
+  @bg-color: rgba(0,0,0,0.1);
+  display: inline-flex;height: 26px;align-items: center;line-height: 1em;padding: 0 9px;background: @bg-color;font-size: 14px;
+  border-radius: 30px;vertical-align: middle;border: 1px solid transparent;
+  &-close{padding-right: 4px;}
+  &-close-btn{font-size: 18px;margin-left: 5px;cursor: pointer;opacity: .4;}
+  &-close-btn:hover{opacity: 1;}
+  &-outline{background: transparent;border-color: @bg-color;}
+  .m-avatar{width: 24px;height: 24px;margin: 0 5px 0 -9px;}
+  &-small{
+    font-size: 12px;height: 22px;padding: 0 7px;
+    &.m-tag-close{padding-right: 2px;}
+    .m-tag{
+      &-close-btn{font-size: 16px;}
+    }
+    .m-avatar{width: 20px;height: 20px;margin: 0 5px 0 -7px;}
+  }
+}
+</style>
